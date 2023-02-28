47773dace24726e6ba45871003c7764c0a8b1aaa
sureyyasenturk <sureyyasenturk7@gmail.com>
2023-02-28 15:36:44 +0300

    nosql

---
+++ src/main/java/service/DersService.java
@@ -0,0 +1,26 @@
+package service;
+
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import entity.Ders;
+import org.bson.Document;
+
+public class DersService {
+    public void addDers(MongoCollection<Document> collection){
+        try {
+            Ders ders= new Ders();
+            ders.setDersAdi("Matematik");
+            ders.setDersKodu("123KS2W");
+            Gson gson=new Gson();
+            BasicDBObject dbObject=BasicDBObject.parse(gson.toJson(ders));
+            Document dersDocument= new Document();
+            dersDocument.append("Kayıt3",dbObject);
+            collection.insertOne(dersDocument);
+            System.out.println("BAŞARILI BİR ŞEKİLDE KAYIT İŞLEMİ YAPILDI");
+
+        }catch (Exception exception){
+            System.out.println("KAYIT OLUŞTURULURKEN BİR HATA OLUŞTU LÜTFEN TEKRAR DENEYİN");
+        }
+    }
+}
